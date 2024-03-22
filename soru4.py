0974054767618369f0831d776721d7d24831c137
selinakcanca <selingd98@gmail.com>
2024-03-22 18:14:33 +0300

    soru çözüldü

--- soru4.py
+++ soru4.py
@@ -1,6 +1,20 @@
 #4- Kullanıcıdan girilen sayının asal sayı olup olmadığını söyleyen bir program yazınız.
-#4- Kullanıcıdan girilen sayının asal sayı olup olmadığını söyleyen bir program yazınız.
 
-def asalSayi 
+#mantık olarak kullanıcnın girdiği sayıyı kendisinden önceki sayılara böleceğiz. 
 
-2 def asalsayi2
+sayi = int(input("Sayı giriniz:"))
+asalmi = 0
+
+for i in range (2,sayi):  # i değeri her döngüde 2 ve kullanıcının girdiği sayı arasındaki değerleri sırayla alacak.  2 yazmanın nedeni: asal sayılar 1 den ve kendisinden başka sayıya bölünmediği için 1'i dışarda bırakmamız lazım.
+                           # son değer döngünün dışında oluyor. örn sayı= 9 i=8 olur yani döngü dışı kalır.
+    if sayi%i == 0:  #sayi bölü i nin kalanı 0 mı?  %: sayıyı bölüp kalanı alır.
+      asalmi += 1 
+      break
+   
+
+if asalmi == 0:
+      print(sayi, "sayısı asaldır.")
+else: 
+      print(sayi, "sayısı asal değildir.")
+      
+                   
