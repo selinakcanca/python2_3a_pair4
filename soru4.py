2ea4e1ddc649589d70f3c5b3117018b421b4a1fe
selinakcanca <selingd98@gmail.com>
2024-03-22 17:59:40 +0300

    asalsayi

--- soru4.py
+++ soru4.py
@@ -1,2 +1,4 @@
 #4- Kullanıcıdan girilen sayının asal sayı olup olmadığını söyleyen bir program yazınız.
 #4- Kullanıcıdan girilen sayının asal sayı olup olmadığını söyleyen bir program yazınız.
+
+def asalSayi 
