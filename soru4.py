15288292479efdf458134f678e7725ca9b17d479
selinakcanca <selingd98@gmail.com>
2024-03-22 18:53:17 +0300

    final

--- soru4.py
+++ soru4.py
@@ -3,12 +3,13 @@
 #mantık olarak kullanıcnın girdiği sayıyı kendisinden önceki sayılara böleceğiz. 
 
 sayi = int(input("Sayı giriniz:"))
-asalmi = 0
+asalmi = 0  #eğer kalan 0 ise girilen sayı asal sayı değldir.
 
 for i in range (2,sayi):  # i değeri her döngüde 2 ve kullanıcının girdiği sayı arasındaki değerleri sırayla alacak.  2 yazmanın nedeni: asal sayılar 1 den ve kendisinden başka sayıya bölünmediği için 1'i dışarda bırakmamız lazım.
-                           # son değer döngünün dışında oluyor. örn sayı= 9 i=8 olur yani döngü dışı kalır.
-    if sayi%i == 0:  #sayi bölü i nin kalanı 0 mı?  %: sayıyı bölüp kalanı alır.
-      asalmi += 1 
+                           # ikinci değer yani son değer döngünün dışında oluyor. örn sayı= 9 i=8 olur yani döngü dışı kalır.
+   
+    if sayi%i == 0:  #sayi bölü i nin kalanı 0 'a eşit mi?  %: sayıyı bölüp kalanı alır.
+      asalmi += 1  #eğer sayı asal değilse değişkenin değerini 1 arttır. Sonda eğer asalmi değişkeninin değeri 0 kaldıysa girilen sayı asal.
       break
    
 
@@ -17,4 +18,9 @@ if asalmi == 0:
 else: 
       print(sayi, "sayısı asal değildir.")
       
-                   
+"""
+P.S: Program çalıştıktan sonra eğer 'asalmi' değişkeninin değeri 0 kaldıysa girilen sayı asaldır, eğer 'asalmi' değişkenin değeri 1 olursa girilen sayı asal değildir . 
+
+'asalmi'değişkeninin değerini kullanıcının girdiği sayı kendisinden ve 1'den başka sayıya tam bölündüğünde değiştiriyoruz.
+
+"""
