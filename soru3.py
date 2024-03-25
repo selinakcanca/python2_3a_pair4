c82919f0d8c647398e6a3ae1025ed550e726a51b
selinakcanca <selingd98@gmail.com>
2024-03-25 14:38:02 +0300

    added another version

--- soru3.py
+++ soru3.py
@@ -10,3 +10,25 @@ ekok=(sayi1*sayi2)// ebob
 
 print("EBOB:", ebob)
 print("EKOK:", ekok)
+
+
+#diğer versiyon
+
+def ebob_ekok(sayi1,sayi2):
+    if sayi1 > sayi2:
+        kucuksayi = sayi2
+    else:
+        kucuksayi = sayi1
+    for i in range(1, kucuksayi+1):
+        if(sayi1 % i ==0) and (sayi2 % i ==0):
+            ebob=i
+
+    ekok =(sayi1 * sayi2) // ebob
+    return ebob, ekok
+
+sayi1 = int(input("Birinci Sayıyı Giriniz: "))
+sayi2 = int(input("İkinci Sayıyı Giriniz: "))
+ 
+ebob, ekok = ebob_ekok(sayi1, sayi2)
+print("EBOB:", ebob)
+print("EKOK:", ekok)
