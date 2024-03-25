7fca2c8493439e1fac0289ab80cfb4a94f64d4af
selinakcanca <selingd98@gmail.com>
2024-03-25 14:37:25 +0300

    update

--- soru3.py
+++ soru3.py
@@ -6,7 +6,7 @@ sayi1=int(input("Birinci Sayıyı Giriniz: "))
 sayi2=int(input("İkinci Sayıyı Giriniz: "))
 
 ebob=math.gcd(sayi1, sayi2)
-ekok=(sayi1*sayi2)/ebob
+ekok=(sayi1*sayi2)// ebob
 
 print("EBOB:", ebob)
 print("EKOK:", ekok)
